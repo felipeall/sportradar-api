c6f6f13c0bfc9e904d3cd0c65404c287a7351577
Felipe Allegretti <felipeallh@gmail.com>
2023-02-23 23:29:32 -0300

    Deploy MkDocs (#2)
    * Implement mkdocs
    
    * Add mkdocs plugins
    
    * Add GitHub docs workflow
    
    * Implement gh-docs command
    
    * Add poetry to GitHub workflow

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# sportradar-api-wrapper
+# Sportradar API Wrapper
 Lightweight wrapper for [Sportradar API](https://developer.sportradar.com/docs/read/Home)
+
+## Set up
+1. Register on [Sportradar Developer](https://developer.sportradar.com/member/register)
+2. Generate an API Key
+
+## Installation
+````bash
+pip install git+https://github.com/felipeall/sportradar-api-wrapper.git
+````
+
+## Usage
+````python
+from sportradar_api_wrapper import SoccerExtended
+sportradar = SoccerExtended(api_key='SPORTRADAR_API_KEY')
+````
--- sportradar_api_wrapper/soccer_extended/soccer_extended.py
+++ sportradar_api_wrapper/soccer_extended/soccer_extended.py
@@ -8,13 +8,40 @@ class SoccerExtended(SportradarAPI):
     api: str = "soccer-extended"
 
     def get_competitions(self) -> dict:
+        """Get all available Soccer competitions.
+
+        Returns:
+            API response
+        """
         return self._call_endpoint(endpoint="competitions", key="competitions")
 
     def get_seasons(self) -> dict:
+        """Get historical season information for all competitions.
+
+        Returns:
+            API response
+        """
         return self._call_endpoint(endpoint="seasons", key="seasons")
 
-    def get_season_summary(self, season_urn: str) -> dict:
+    def get_season_summaries(self, season_urn: str) -> dict:
+        """Get the summaries for all sport events in a season (any status). Provides information for all matches from a
+        given season including scoring and statistics at the match level.
+
+        Args:
+            season_urn:
+
+        Returns:
+            API response
+        """
         return self._call_endpoint(endpoint=f"seasons/{season_urn}/summaries", key="summaries")
 
     def get_season_competitors(self, season_urn: str) -> dict:
+        """Get all teams participating for a given season.
+
+        Args:
+            season_urn: URN of a given season
+
+        Returns:
+            API response
+        """
         return self._call_endpoint(endpoint=f"seasons/{season_urn}/competitors", key="season_competitors")
