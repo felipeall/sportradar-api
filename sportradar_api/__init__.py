40b37f67470564d5ab883fc05290421b9b2a242b
Felipe Allegretti <felipeallh@gmail.com>
2023-02-25 11:31:35 -0300

    Create SoccerExtendedPandas module

--- sportradar_api/__init__.py
+++ sportradar_api/__init__.py
@@ -1,3 +1,4 @@
-from sportradar_api.soccer_extended.soccer_extended import SoccerExtended
+from .soccer_extended.soccer_extended import SoccerExtended
+from .soccer_extended.soccer_extended_pandas import SoccerExtendedPandas
 
-__all__ = ["SoccerExtended"]
+__all__ = ["SoccerExtended", "SoccerExtendedPandas"]
