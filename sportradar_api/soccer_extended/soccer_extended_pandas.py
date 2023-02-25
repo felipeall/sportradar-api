fd7f6fd991af1f99ada3e67424f875a4efa00ca2
Felipe Allegretti <felipeallh@gmail.com>
2023-02-25 12:15:52 -0300

    Add Competitions and Seasons to SoccerExtendedPandas

--- sportradar_api/soccer_extended/soccer_extended_pandas.py
+++ sportradar_api/soccer_extended/soccer_extended_pandas.py
@@ -16,6 +16,42 @@ class SoccerExtendedPandas:
     def __post_init__(self):
         self.soccer_extended = SoccerExtended(api_key=self.api_key, quiet=self.quiet)
 
+    def get_competitions(self) -> pd.DataFrame:
+        """Get all available Soccer competitions.
+
+        Returns:
+            Pandas DataFrame
+        """
+        competitions = self.soccer_extended.get_competitions()
+        competitions = pd.json_normalize(competitions["competitions"])
+
+        return competitions
+
+    def get_seasons(self) -> pd.DataFrame:
+        """Get historical season information for all competitions.
+
+        Returns:
+            Pandas DataFrame
+        """
+        seasons = self.soccer_extended.get_seasons()
+        seasons = pd.json_normalize(seasons["seasons"])
+
+        return seasons
+
+    def get_season_competitors(self, season_urn: str) -> pd.DataFrame:
+        """Get all teams participating for a given season.
+
+        Args:
+            season_urn: URN of a given season
+
+        Returns:
+            Pandas DataFrame
+        """
+        season_competitors = self.soccer_extended.get_season_competitors(season_urn=season_urn)
+        season_competitors = pd.json_normalize(season_competitors["season_competitors"])
+
+        return season_competitors.assign(season_urn=season_urn)
+
     def get_player_profile_info(self, player_urn: str) -> pd.DataFrame:
         """Get the basic information from a player profile
 
